7ab4815ae7452907595b13a45d0de1bc127b6411
ECore3Core <150679700+ECore3Core@users.noreply.github.com>
2024-10-29 21:30:49 +0400

    Задание 3

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
 import Reflection.Reflection;
 import Vehicle.Vehicle;
+import Exceptions.DuplicateModelNameException;
+import Exceptions.NoSuchModelNameException;
 import MainVehicles.*;
 
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args)throws NoSuchModelNameException, DuplicateModelNameException{
         //Задание 1
         //Reflection.SetPriceByName(args[0], args[1], args[2], args[3]);
 
@@ -12,5 +14,11 @@ public class App {
         Vehicle v = Vehicles.createVehicle("BMW", 10, car);
         System.out.println(v);
 
+        //Задание 3
+        Scooter scooter = new Scooter("Suzuki", 10);
+        scooter.printInfo();
+        scooter.deleteModel("Scooter2");
+        scooter.setPrice("Scooter4", 50);
+        scooter.printInfo();
     }
 }
