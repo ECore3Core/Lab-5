77766debf922ae12daa971d54fe0d683186a575f
ECore3Core <150679700+ECore3Core@users.noreply.github.com>
2024-11-02 03:48:38 +0400

    Задание 5
    тесты

--- src/App.java
+++ src/App.java
@@ -15,11 +15,24 @@ public class App {
         System.out.println(v);
 
         //Задание 3
-        Scooter scooter = new Scooter("Suzuki", 10);
+        Scooter scooter = new Scooter("LADA", 10);
         scooter.printInfo();
-        scooter.deleteModel("Scooter2");
-        scooter.setPrice("Scooter4", 50);
+        scooter.deleteModel("QuadBike1");
         scooter.printInfo();
+        scooter.setModelName("QuadBike4", "kal");
+        scooter.setPrice("QuadBike2", 15);
+        scooter.printInfo();
+        scooter.addModel("bab", 5);
+        scooter.printInfo();
+        String[] scooterNames = scooter.getModelsNames();
+        for(String s : scooterNames){
+            System.out.print(s + " ");
+        }
+        double[] scooterPrices = scooter.getModelsPrices();
+        for(double d : scooterPrices){
+            System.out.print(d + " ");
+        }
+        System.out.println();
 
         //Задание 4
         QuadBike quadBike = new QuadBike("Yamaha", 10);
@@ -42,7 +55,7 @@ public class App {
         System.out.println();
 
         //Задание 5
-        Moped moped = new Moped("Yamaha", 10);
+        Moped moped = new Moped("Mercedes", 10);
         moped.printInfo();
         moped.deleteModel("Moped1");
         moped.printInfo();
@@ -55,5 +68,10 @@ public class App {
         for(String s : mopedNames){
             System.out.print(s + " ");
         }
+        double[] mopedPrices = moped.getModelsPrices();
+        for(double d : mopedPrices){
+            System.out.print(d + " ");
+        }
+        System.out.println();
     }
 }
